231d5ef1dee990c4f51f1cd2cf79ea2dbc9b7ca5
rluver <skyd0608@gmail.com>
2022-06-16 08:19:55 +0900

    fix: Add f_tr mapping

--- tensorflow/layers/squeeze_and_excitation_networks.py
+++ tensorflow/layers/squeeze_and_excitation_networks.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Layer, GlobalAveragePooling1D, Dense
+from tensorflow.keras.layers import Layer, GlobalAveragePooling1D, Dense, Conv1D
 
 
 class SqueezeAndExcitationNetworks(Layer):
@@ -8,11 +8,14 @@ class SqueezeAndExcitationNetworks(Layer):
         self.reduction_ratio = reduction_ratio
 
     def call(self, x):
-        average_pooling = GlobalAveragePooling1D()(x)
-        fc1 = Dense(units=(x.shape[-1] // self.reduction_ratio), activation='relu')(average_pooling)
-        fc2 = Dense(units=(x.shape[-1]), activation='sigmoid')(fc1)
+        filters=x.shape[-1]
+
+        u = Conv1D(filters=filters, kernel_size=x.shape[1])(x)
+        average_pooling = GlobalAveragePooling1D()(u)
+        fc1 = Dense(units=(filters // self.reduction_ratio), activation='relu')(average_pooling)
+        fc2 = Dense(units=(filters), activation='sigmoid')(fc1)
 
         fc2_repeated = tf.repeat(fc2, repeats=x.shape[1], axis=0)
         fc2_for_multiply = tf.reshape(fc2_repeated, shape=(-1, *x.shape[1:]))
 
-        return x * fc2_for_multiply
+        return u * fc2_for_multiply
